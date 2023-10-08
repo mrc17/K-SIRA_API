5ea17e81005316ad3dfd7c794945a4b128db1f9c
Axel Toure <axeltoure1eme@gmail.com>
2023-10-08 12:20:54 +0000

    Creation de la Table branches d'activites

---
+++ database/migrations/2023_10_08_121811_branches_d_activites.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('branches_d_activites', function (Blueprint $table) {
+            $table->string('id', 10)->primary();
+            $table->string('nom_de_la_branche');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches_d_activites');
+    }
+};
